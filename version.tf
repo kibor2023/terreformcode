a9133e0ad0d69111194937d355e914ff1db47615
kibor2023 <katekibor83@yahoo.com>
2023-09-17 19:56:36 -0500

    change to version

--- version.tf
+++ version.tf
@@ -1,7 +1,11 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashi corp"
+        source = "hashicorp/aws"
+        version = "4.61.0"
     }
   }
+}
+provider "aws" {
+  region = "us-east-1"
 }
